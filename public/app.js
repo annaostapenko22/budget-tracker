56843497c50859da66d80846e3003f63fe76f9a0
Anna Ostapenko <kvintabudinvest@gmail.com>
2020-10-24 22:28:49 +0200

    added class invoice

--- public/app.js
+++ public/app.js
@@ -1,4 +1,21 @@
 "use strict";
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        // what counstructor receives and assign to fields
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes $" + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invOne = new Invoice('anna', "work on work", 250);
+var invTwo = new Invoice('alyona', "work in kiev", 250);
+console.log(invOne, invTwo);
+var invoices = [];
+invoices.push(invOne);
 var form = document.querySelector(".new-item-form");
 var type = document.querySelector("#type");
 var tofrom = document.querySelector("#tofrom");
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,33 @@
+class Invoice {
+    // class fileds
+    readonly client: string;
+    private details: string;
+    public amount: number;
+
+    constructor(c: string, d: string, a: number) {
+        // what counstructor receives and assign to fields
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`
+    }
+}
+
+const invOne = new Invoice('anna', "work on work", 250)
+const invTwo = new Invoice('alyona', "work in kiev", 250)
+console.log(invOne, invTwo)
+let invoices: Invoice[] = [];
+invoices.push(invOne)
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 const type = document.querySelector("#type") as HTMLSelectElement;
 const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
-form.addEventListener("submit", (e: Event)=> {
+form.addEventListener("submit", (e: Event) => {
     e.preventDefault()
 
     console.log(type.value,
